a6762e99b1101aedd52beb86dd2c4f94bb89984a
Thomas Beckford <tebeckford@gmail.com>
2023-04-26 01:37:07 -0300

    feat: add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,44 @@
 import "@/src/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GMT_ID}');
+      `}
+          </Script>
+
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GMT_ID}`}
+          />
+
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GMT_ID}', {
+        page_path: window.location.pathname,
+      });
+    `,
+            }}
+          />
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
