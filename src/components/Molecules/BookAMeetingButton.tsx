260911810535ed621a68333115875831ffa30cde
Thomas Beckford <tebeckford@gmail.com>
2023-06-14 17:35:18 -0300

    fix: calendly

--- src/components/Molecules/BookAMeetingButton.tsx
+++ src/components/Molecules/BookAMeetingButton.tsx
@@ -4,7 +4,7 @@ export default function BookAMeetingButton() {
   return (
     <Button
       onClick={() => {
-        window.open("https://calendly.com/tebeckford/15min", "_blank");
+        window.open("https://calendly.com/tebeckford/30min", "_blank");
       }}
       className="bg-indigo-500 hover:bg-indigo-600 rounded-lg border-none text-white drop-shadow-lg transition-all"
     >
