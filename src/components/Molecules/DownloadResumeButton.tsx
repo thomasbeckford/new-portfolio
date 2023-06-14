29f764d5eba65db023e428c57f097ee912a902f7
Thomas Beckford <tebeckford@gmail.com>
2023-06-14 14:52:37 -0300

    fix

--- src/components/Molecules/DownloadResumeButton.tsx
+++ src/components/Molecules/DownloadResumeButton.tsx
@@ -3,12 +3,12 @@ import Button from "../Atoms/Button";
 export default function DownloadResumeButton() {
   const handleDownload = async () => {
     try {
-      const response = await fetch("/Resume-Thomas-Beckford-2023.pdf");
+      const response = await fetch("/Thomas_Resume_2023.pdf");
       const blob = await response.blob();
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
-      link.download = "Resume-Thomas-Beckford-2023.pdf";
+      link.download = "Thomas_Resume_2023.pdf";
       link.click();
       URL.revokeObjectURL(url);
     } catch (error) {
